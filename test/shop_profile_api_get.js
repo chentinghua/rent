904721d6077a8f51f4ebe5cc373c987ba3691642
TinghuaC <james@tarad.com>
2016-03-07 17:12:43 +0700

    init

---
+++ test/shop_profile_api_get.js
@@ -0,0 +1,32 @@
+var request = require('request');
+var expect = require('chai').expect;
+
+describe('API Unit Test : shop profile ', function() {
+	
+	var url = 'http://10.127.4.69:3000/shop/profiles/1';
+	var len = 0;
+	
+    before(function(done){
+    	
+    	request(url, function (error, response , body) {
+			
+			 if (!error &&  response.statusCode == 200) {
+				var response = JSON.parse(body);
+				len = response.data.length; 
+			 } 
+			 
+			 done();
+			 
+		});
+    	
+    	
+    });
+	 
+     
+     it('API response count should be equals 1', function(done) {
+    	 expect(len).to.be.equal(1);
+	     done();
+     });	
+		   
+});
+
